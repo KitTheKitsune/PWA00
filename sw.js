58838e719066edd38e6e4816fb5d3f1ec9110f21
KokatsuKitsune <kendricksmith@creighton.edu>
2021-05-06 08:15:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -34,7 +34,7 @@ self.addEventListener('activate',event => {
 //the fetch handler
 self.addEventListener('fetch',function(event) {
    event.respondWith(
-       cahes.match(event.request).then(function(response){
+       caches.match(event.request).then(function(response){
           return response || fetch(event.request);
        })
      );
