e1e5cb7a16635a66b9bc149a05a87956b1841866
KokatsuKitsune <kendricksmith@creighton.edu>
2021-05-08 16:30:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function kToC(x){
 }
 
 function kToF(x){
-  return (kToC(x) * 9)/5 + 32;
+  return Math.round(((kToC(x) * 9)/5 + 32) * 100) / 100;
 }
 
 const app = document.getElementById('root');
@@ -28,28 +28,34 @@ container.setAttribute('class','container');
 app.appendChild(container);
 
 let request = new XMLHttpRequest();
-let city = ["London"];
+let cities = ["London","France","Tokyo","NewYork"];
 let api = dcrypt("1e5ae6831a0fa16012e92d525385e2f5");
 
-request.open('GET','https://api.openweathermap.org/data/2.5/weather?q='+ city[0] +'&appid=' + api,true);
-request.send();
-request.onload = function(){
-  var data = JSON.parse(this.response);
-  console.log(data);
-  if (request.status>=200 && request.status<400){
-     const card = document.createElement('div');
-     card.setAttribute('class','card');
-    const h1 = document.createElement('h1');
-    h1.textContent = data.name;
-    const p = document.createElement('p');
-    p.textContent = kToF(data.main.temp) + " F";
-    
-    container.appendChild(card);
-    card.appendChild(h1);
-    card.appendChild(p);
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = "Dangit, it's not working!";
-    app.appendChild(errorMessage);
-  } //END else
-} //END function
+function getCityData(city){
+  request.open('GET','https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=' + api,true);
+  request.send();
+  request.onload = function(){
+    var data = JSON.parse(this.response);
+    console.log(data);
+    if (request.status>=200 && request.status<400){
+       const card = document.createElement('div');
+       card.setAttribute('class','card');
+      const h1 = document.createElement('h1');
+      h1.textContent = data.name;
+      const p = document.createElement('p');
+      p.textContent = kToF(data.main.temp) + " F";
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    } else {
+      const errorMessage = document.createElement('marquee');
+      errorMessage.textContent = "Dangit, it's not working!";
+      app.appendChild(errorMessage);
+    } //END else
+  } //END function
+}//END function
+
+for (let i = 0; i < cities.length; i++){
+  getCityData(cities[i]);
+}
