392f10d3cc22437e163c734f09ffac98bbbd1111
KokatsuKitsune <kendricksmith@creighton.edu>
2021-05-08 16:01:23 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,39 @@ function regSW(){
     navigator.serviceWorker.register('./sw.js');
   }
 }
+
+const app = document.getElementById('root');
+//const logo = document.createElement('img');
+//logo.src = 'images/logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class','container');
+
+//app.appendChild(logo);
+app.appendChild(container);
+
+let request = new XMLHttpRequest();
+let city = "London";
+let api = "";
+request.open('GET','https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=' + api,true);
+request.send();
+request.onload = function(){
+  var data = JSON.parse(this.response);
+  console.log(data);
+  if (request.status>=200) && request.status<400){
+     const card = document.createElement('div');
+     card.setAttribute('class','card');
+    const h1 = document.createElement('h1');
+    h1.textContent = data.name;
+    const p = document.createElement('p');
+    p.textContent = data.main.temp;
+    
+    container.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(p);
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = "Dangit, it's not working!";
+    app.appendChild(errorMessage);
+  } //END else
+} //END function
